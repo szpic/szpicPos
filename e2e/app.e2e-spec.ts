c48588084fb359e330e5a257cb12469261b724ac
szpic <szpicus@interia.pl>
2017-04-09 16:48:51 +0200

    Fixing refences in package.json

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -2,14 +2,13 @@ import { browser, element, by } from 'protractor';
 
 describe('QuickStart E2E Tests', function () {
 
-  let expectedMsg = 'Angular Router';
+  let expectedMsg = 'Login';
 
   beforeEach(function () {
     browser.get('');
   });
 
   it('should display: ' + expectedMsg, function () {
-    expect(element(by.css('h1')).getText()).toEqual(expectedMsg);
+    expect(element(by.css('h2')).getText()).toEqual(expectedMsg);
   });
-
 });
